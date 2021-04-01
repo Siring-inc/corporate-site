897af332111e5bdb09be192fc56f05b04838b02b
5Sense-HuynhPhuc <huynh_phuc@vn.5sense.co.jp>
2021-04-01 17:19:06 +0700

    [feature_upgrade_service_page] コメント表示

--- components/molecules/serviceComment.vue
+++ components/molecules/serviceComment.vue
@@ -19,19 +19,26 @@ export default {
 .service-header__comment {
   position: absolute;
   display: inline-block;
-  // margin: 1.5em 15px 1.5em 0;
   margin: -0.9em -2px 5.5em -254px;
-  padding: 17px 5px;
-  width: 70px;
-  height: 70px;
-  line-height:13px;
+  padding: 1.7rem 0.5rem;
+  width: 7rem;
+  height: 7rem;
+  line-height:1.3rem;
   text-align: center;
   color: #FFF;
-  font-size: 12px;
+  font-size: 1.2rem;
   font-weight: bold;
   background: $red;
   border-radius: 50%;
   box-sizing: border-box;
+  @include max-screen($mobile-break-point) {
+    // margin: -0.9em -2px 5.5em -182px;
+    margin: -0.9em -2px 5.5em -169px;
+    width: 6rem;
+    height: 6rem;
+    line-height:1rem;
+    font-size: 1rem;
+  }
 }
 .service-header__comment:before {
   content: "";
@@ -40,11 +47,17 @@ export default {
   right: -14px;
   margin-top: -15px;
   border: 10px solid transparent;
-  // border: 15px solid transparent;
   border-left: 25px solid $red;
-  // border-left: 15px solid $red;
   z-index: 0;
   -webkit-transform: rotate(45deg);
   transform: rotate(45deg);
+  @include max-screen($mobile-break-point) {
+    bottom: 4px;
+    right: -14px;
+    margin-top: -15px;
+    border: 10px solid transparent;
+    border-left: 25px solid $red;
+    transform: rotate(38deg);
+  }
 }
 </style>
